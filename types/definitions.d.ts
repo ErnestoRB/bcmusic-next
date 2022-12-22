b493342ee3bc2354ff02d49191693897c386772a
Ernesto Ramírez <ernestorb_@outlook.com>
2022-12-22 10:13:47 -0600

    Añadir tipos TS

---
+++ types/definitions.d.ts
@@ -0,0 +1,67 @@
+export interface SpotifyData {
+  error?: string;
+  error_description?: string;
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+}
+
+export interface SpotifyTopArtistData {
+  items: SpotifyArtist[];
+}
+
+export interface SpotifyArtist {
+  external_urls: {
+    spotify: string;
+  };
+  followers: {
+    href: string;
+    total: number;
+  };
+  genres: string[];
+  href: string;
+  id: string;
+  images: [
+    {
+      url: string;
+      height: number;
+      width: number;
+    }
+  ];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+}
+
+export interface PaisFields {
+  ID: string;
+  Nombre: string;
+  Nombre_Corto: string;
+}
+
+export interface SignUpInput {
+  Email: string;
+  Apellido: string;
+  Nombre: string;
+  Contraseña: string;
+  Nacimiento: Date;
+  Pais: number;
+}
+
+export interface Usuario {
+  ID: string;
+  Nombre: string;
+  Apellido: string;
+  Usuario: string;
+  Contraseña?: string;
+  Nacimiento: Date;
+  Pais: string;
+}
+
+type ResponseData = {
+  message: string;
+  data?: any;
+};
---
+++ types/next-auth.d.ts
@@ -0,0 +1,32 @@
+import NextAuth, { DefaultSession, DefaultUser, Profile } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      id: string;
+      name?: string;
+      apellido?: string;
+      email: string;
+      image?: string;
+      nacimiento?: string;
+      pais?: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    name?: string;
+    apellido?: string;
+    email: string;
+    image?: string;
+    nacimiento?: string;
+    idPais?: string;
+  }
+
+  interface SpotifyProfile extends Profile {
+    images: { url: string }[];
+  }
+}
