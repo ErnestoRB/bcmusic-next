c8c55d15b61bd0477aa56a01368a3573609f5c82
Ernesto Ramírez <ernestorb_@outlook.com>
2023-04-19 15:09:24 -0600

    Añadir snippet de como copiar archivos a build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// const CopyPlugin = require("copy-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,6 +13,19 @@ const nextConfig = {
       },
     ],
   },
+  /* webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+     if (isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [{ from: "vm", to: "pages/" }],
+        })
+      );
+    } 
+    return config;
+  },*/
 };
 
 module.exports = nextConfig;
