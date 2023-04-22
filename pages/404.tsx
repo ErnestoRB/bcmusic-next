002b1e652c409b34d635216c6aaf202883c93389
Ernesto Ramírez <ernestorb_@outlook.com>
2023-04-22 00:50:41 -0600

    Agregar página 404 personalizada

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import notFound from "../images/not-found.gif";
+
+export default function NotFound() {
+  return (
+    <div className="grid place-items-center w-full">
+      <div className="w-max flex flex-col items-center gap-2">
+        <h1 className="text-center text-3xl">Página no implementada!</h1>
+        <Image
+          src={notFound}
+          alt="Page wasn't found!"
+          className="w-32 md:w-64 lg:w-72"
+        ></Image>
+      </div>
+    </div>
+  );
+}
