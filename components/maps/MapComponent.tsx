a96132ac9e8f63d7ec00b9bf403559ffe8c56c6c
Ernesto Ramírez <ernestorb_@outlook.com>
2024-09-08 12:40:02 -0600

    Agregar coordenadas de fallback (error al cargar mapa)

--- components/maps/MapComponent.tsx
+++ components/maps/MapComponent.tsx
@@ -21,12 +21,25 @@ export default function MapComponent({ routeGeometry, location }: MapProps) {
 
   return (
     <>
-      <MapContainer center={location} zoom={13} className="w-full h-full">
+      <MapContainer
+        center={location ?? [21.913380555556, -102.315025]}
+        zoom={13}
+        className="w-full h-full"
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Polyline positions={routePoints} color="blue" />
+        {/*   <TileLayer
+          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+        /> */}
+        {/*     <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          subdomains={"abcd"}
+        /> */}
+        {routePoints && <Polyline positions={routePoints} color="blue" />}
         {location && (
           <Marker position={location}>
             <Popup>Te encuentras aqui</Popup>
